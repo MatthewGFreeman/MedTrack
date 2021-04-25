c0f4922f4083e5c58731a719b2d359a6e873367f
Matthew Freeman <matthew.freeman@revature.net>
2021-04-25 18:06:31 -0400

    Attempts to add logging. Does not work currently

--- lib/src/main/java/Medtrack/DatabaseConnection.java
+++ lib/src/main/java/Medtrack/DatabaseConnection.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class DatabaseConnection {
     private final String url = "jdbc:postgresql://localhost:5432/medtrack";
     private final String username = "medtrack";
